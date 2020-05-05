592bad66c40aa3048152176af4927d5c638f86ba
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-05 17:01:35 -0400

    index updated

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require("express"); 
-
+const postRouter = require('./posts/postrouter');
 const server = express();
 
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.json({ api: 'Wow'})
+} )
