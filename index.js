33dca1dc49d7f3f94d0dda1d1f3aa7d64d0a3a78
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-05 17:53:53 -0400

    server.use updated

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const server = express();
 
 server.use(express.json());
 
-server.get('/', (req, res) => {
-    res.json({ api: 'Wow'})
-} )
+
+server.get("/", (req, res) => {
+    res.json({ query: req.query, params: req.params, headers: req.headers });
+  });
+  
+  server.use("/api/posts", postRouter);
+  
+  server.listen(4000, () => {
+    console.log("\n*** Server Running on http://localhost:4000 ***\n");
+  });
